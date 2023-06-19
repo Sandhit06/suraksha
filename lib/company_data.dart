39df37d61658bbec4a20dc9fc63def925c1f7885
Sandhit <sandhitkarmakar@gmail.com>
2023-06-19 23:42:44 +0530

    Update changes

---
+++ lib/company_data.dart
@@ -0,0 +1,9 @@
+class CompanyData
+{
+  static String defaultPassword = 'qwertyuiop';
+  static String defaultRole = 'employee';
+  static Map<String, dynamic> office = {
+    'latitude': 12.84030,
+    'longitude': 80.15418,
+  };
+}
